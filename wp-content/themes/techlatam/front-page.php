41f156370e4fe0d42b4aa940c2a7f5c917f5bc77
karina <kael_guzman@hotmail.com>
2018-08-25 22:02:56 -0600

    creacion de cuerpo de proyecto

--- wp-content/themes/techlatam/front-page.php
+++ wp-content/themes/techlatam/front-page.php
@@ -1,7 +1,76 @@
 <?php get_header(); ?>
 
     <div class="row">
-        <h1>contenido</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-12" id="div-photografy">
+                    <h4 id="letra-photodiary">PHOTODIARY</h4>
+                    <h1 id="letra-perfect-weekend">The perfect weekend getaway</h1>
+                    <p id="letra-parrafo">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos sed excepturi fuga quaerat totam dicta doloremque sint nostrum eveniet id laboriosam ipsum voluptatum magni, aperiam cumque quibusdam natus veritatis libero.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet amet fugiat est similique, necessitatibus quasi provident, laudantium impedit tempore pariatur voluptate atque hic placeat, tenetur laboriosam quae aliquam aut obcaecati.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, aspernatur pariatur ut voluptas error sint tenetur! Sunt sed aliquid voluptatibus dicta, earum voluptatum ipsa dolore exercitationem, repellendus nulla labore reprehenderit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas dignissimos hic non mollitia quidem sit esse consectetur? Ex quibusdam illum unde ratione ipsam similique, consequatur dolorem reiciendis, atque iure facilis?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet iure earum dolorum aperiam facilis necessitatibus reprehenderit, beatae deserunt error facere perspiciatis incidunt quae ut deleniti temporibus cumque odio tenetur laboriosam.
+                        
+                    </p>
+                    <h4 id="letra-leave">LEAVE A COMENT</h4>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-12 col-md-6" id="div-lifestyle">
+                    <img src="<?php echo THEMEROOT; ?>/img/levanta-mano.jpg" alt="">
+                        <h4 id="letra-lifestyle">LIFESTYLE</h4>
+                        <h1 id="letra-bajo-lifestyle">More than just a music festival</h1>
+                        <p id="letra-parrafo">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque illum illo porro error molestiae. Corrupti temporibus obcaecati provident ullam deserunt facere eligendi nulla, unde quam placeat officia ex asperiores vitae?
+                        </p>
+                </div>
+                <div class="col-12 col-md-6" id="div-lifestyle">
+                    <img src="<?php echo THEMEROOT; ?>/img/cafe.jpg" alt="">
+                        <h4 id="letra-lifestyle">LIFESTYLE</h4>
+                        <h1 id="letra-bajo-lifestyle">Life tastes better with coffee</h1>
+                        <p id="letra-parrafo">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptates voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias error aliquam odit laudantium minima quisquam. Ab, neque.
+                        </p>
+                </div>
+                
+            </div>
+
+
+            <div class="row">
+                <div class="col-12 col-md-6" id="div-lifestyle">
+                    <img src="<?php echo THEMEROOT; ?>/img/puente.jpg" alt="">
+                        <h4 id="letra-photodiary">PHOTODIARY</h4>
+                        <h1 id="letra-bajo-photodiary">American dream</h1>
+                        <p id="letra-parrafo">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque illum illo porro error molestiae. Corrupti temporibus obcaecati provident ullam deserunt facere eligendi nulla, unde quam placeat officia ex asperiores vitae?
+                        </p>
+                </div>
+                <div class="col-12 col-md-6" id="div-lifestyle">
+                    <img src="<?php echo THEMEROOT; ?>/img/gorro.jpg" alt="">
+                        <h4 id="letra-photodiary">PHOTODIARY</h4>
+                        <h1 id="letra-bajo-photodiary">A day exploring the Alps</h1>
+                        <p id="letra-parrafo">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptates voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias error aliquam odit laudantium minima quisquam. Ab, neque.
+                        </p>
+                </div>
+            </div>
+
+
+                <div class="row" id="div-email">
+                    <div class="col-12 col-md-12" id="cuadro-email">
+                        <p id="letra-div-email">
+                            Sign up for our newsletter!
+                        </p>
+                        <p id="letra-parrafo-email">Enter a valid email address</p>
+                        <hr>
+                    </div>
+                </div>
+            
+        </div>
     </div>
 
 <?php get_footer(); ?>
--- wp-content/themes/techlatam/header.php
+++ wp-content/themes/techlatam/header.php
@@ -12,23 +12,26 @@
 <div class="container">
     <!-- As a link -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light" id="menu-principal">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#"> <img src="<?php echo THEMEROOT; ?>/img/minimo-logo.jpg" alt=""> </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">HOME</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">LIFESTYLE</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">PHOTODIARY</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">MUSIC</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">TRAVEL</a>
       </li>
     </ul>
   </div>
@@ -37,7 +40,7 @@
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16572027e03%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16572027e03%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22217.7%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="First slide">
+      <img class="d-block w-100" src="<?php echo THEMEROOT; ?>/img/banner.png" alt="First slide">
     </div>
   </div>
 </div>
