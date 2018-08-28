3f80067f1a37bb7a1e8113ed645e4f9607121cc5
karina <kael_guzman@hotmail.com>
2018-08-27 22:43:23 -0600

    segunda pagina proyecto

--- wp-content/themes/techlatam/page.php
+++ wp-content/themes/techlatam/page.php
@@ -1,9 +1,114 @@
 <?php get_header(); ?>
 
-    <div class="row">
-        <div class="container">
-            <h1>hola mundo</h1>
-        </div>
+    <div class="row" id="">
+                    <div class="col-12 col-md-12" id="parrafo-segpag">
+                        <div id="">
+                            <h4 id="letra-photodiary-pag2">PHOTODIARY</h4>
+                        </div>
+                        <div id="">
+                            <h1 id="letra-bajo-lifestyle">The perfect weekend wetaway</h1>
+                        </div>
+
+                        <p id="letra-parrafo-pag2">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptates voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias odit laudantium minima.
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptates voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias odit laudantium minima.
+                            Lorem, ipsum dolor <b>sit amet consectetur adipisicing elit.</b> Laborum, voluptates voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias odit laudantium minima.
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptates voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias odit laudantium minima.
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptates voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias odit laudantium minima.
+                            <br>
+                            <br>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum excepturi officiis illo obcaecati at hic vero quaerat, nesciunt consequatur ducimus deserunt ad iure eos similique cum accusamus eius ratione eum!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ex eaque pariatur odit saepe expedita voluptatibus beatae laboriosam quibusdam facilis temporibus atque quos id, autem, excepturi adipisci. Adipisci, aut rerum.
+                        </p>
+
+                     <div id="img-mantilla">
+                        <img src="<?php echo THEMEROOT; ?>/img/levanta-mantilla.jpg" alt="">
+                     </div> 
+
+                     <div id="img-montana">
+                        <img src="<?php echo THEMEROOT; ?>/img/montana.jpg" alt="">
+                     </div>  
+                        
+                    <div id="img-falda">
+                        <img src="<?php echo THEMEROOT; ?>/img/falda.jpg" alt="">
+                    </div>
+                        
+
+                        <p id="letra-parrafo-cursivo">
+                           <em>"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptates voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias odit laudantium minima.
+                             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptates voluptas totam eius consectetur.
+                              ".</em> 
+                             
+                        </p>
+
+                        <p id="letra-parrafo2-pag2">
+                            Laborum, voluptates voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias odit laudantium minima.
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, voluptates voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias odit laudantium minima.
+                            Lorem, ipsum dolor <b>sit amet consectetur adipisicing elit. Laborum, voluptates voluptas </b> totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias odit laudantium minima.
+                            voluptas totam eius consectetur enim, inventore molestiae dignissimos maxime, et fugiat molestias odit laudantium minima.
+                            Lorem, ipsum dolor sit amet  dignissimos maxime, et fugiat molestias odit laudantium minima.
+                        </p>
+
+
+                        <div id="redes-pag2">
+                            
+                             <ul>
+                                <li>
+                                    SHARE
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                <a href="#">
+                                        <i class="fab fa-google-plus-g"></i>
+                                </a>
+                                </li>
+                                <li>
+                                <a href="#">
+                                        <i class="fab fa-pinterest"></i>
+                                </a>
+                                </li>
+                            </ul>
+                        </div>
+
+
+                        <div class="row" id="">
+                    <div class="col-12 col-md-12" id="cuadro-you-may">
+                        <div id="you-may">
+                            YOU MAY ALSO LIKE
+                        </div>
+                        <div id="contenedores-img">
+                            <div id="contenedor-imgs">
+                                  
+                               <div id="cont-img-CALLE" class="imgsss">
+                                    <img class="imgss" src="<?php echo THEMEROOT; ?>/img/calle.jpg" alt="">
+                                    <h3 class="style-h3">Cold winter days</h3>
+                               </div>
+                               <div id="cont-img-GORRO" class="imgsss">
+                                    <img class="imgss" src="<?php echo THEMEROOT; ?>/img/gorro.jpg" alt="">
+                                    <h3 class="style-h3">A day exploring the Alps</h3>
+                               </div>
+                               <div id="cont-img-PUENTE" class="imgsss">
+                                    <img class="imgss" src="<?php echo THEMEROOT; ?>/img/puente.jpg" alt="">
+                                    <h3 class="style-h3">American dream</h3>
+                               </div>
+                            </div>
+                        
+                        </div>
+                        
+                    </div>
+                </div>
+
+                        
+                    </div>
     </div>
 
 <?php get_footer(); ?>
